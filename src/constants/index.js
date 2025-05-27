217f12ec8c2dbb9e54147b0f132a90b69575ab16
Vijay <vizchan99@gmail.com>
2025-05-27 05:40:51 -0500

    small changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `Problem Solver. Adaptive. Forever Student`;
 
